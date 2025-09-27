c5824e1604ed954030dd9dec596c3426b43b6317
PeanutJar <dexpaclebar@gmail.com>
2025-09-27 00:00:23 -0700

    bullets are removed when off screen

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -5,18 +5,29 @@ public class BulletScript : Projectile
     public float speed = 10f;
     public float speedmultiplier = 1f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Camera cam;
+    private float halfHeight;
+    private float halfWidth;
+
     void Start()
     {
         this.gameObject.tag = "Projectile";
         movercomponent = GetComponent<ProjectileMover>();
         healthcomponent = GetComponent<Health>();
+        cam = Camera.main;
+        halfHeight = Camera.main.orthographicSize;
+        halfWidth = halfHeight * Camera.main.aspect;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        print(transform.position);
+        if(transform.position.y > (cam.transform.position.y + halfHeight) || transform.position.y < (cam.transform.position.y - halfHeight) || 
+            transform.position.x > (cam.transform.position.x + halfWidth) || transform.position.x < (cam.transform.position.x - halfWidth))
+        {
+            gameObject.GetComponent<DeathDestroy>().Die();
+        }
     }
 
     public override float GetSpeed()
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,5 +4,6 @@ public abstract class Projectile : MonoBehaviour
 {
     public ProjectileMover movercomponent;
     public Health healthcomponent;
+    public int lifetimeduration;
     public abstract float GetSpeed();
 }
