3dfe81a9331e60fc2df16305a207f2da29f341c4
PeanutJar <dexpaclebar@gmail.com>
2025-10-01 16:39:32 -0700

    added certain ui image assets

--- Assets/Scripts/GneralScript.cs
+++ Assets/Scripts/GneralScript.cs
@@ -16,6 +16,7 @@ public class GneralScript : MonoBehaviour
     public GameObject meteorprefab;
 
     private float timecount;
+    public int score;
 
     //[Header("GameData")]
     // Start is called once before the first execution of Update after the MonoBehaviour is created
